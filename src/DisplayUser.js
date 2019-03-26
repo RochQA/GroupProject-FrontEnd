a1c395cf858b8c513972263f6e05ab18582e1428
Roch <roch.sherriff@academytrainee.com>
2019-03-26 15:45:17 +0000

    Improved routing

--- src/DisplayUser.js
+++ src/DisplayUser.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
+import {
+    Route,
+    Link,
+    NavLink,
+    HashRouter
+  } from "react-router-dom";
 import './App.css';
 
 class DisplayUser extends Component{
     render(){
         return(
             <div className="userDisplay">
+            <HashRouter>
                 <p>User: {this.props.user}</p>
-                <button type="button" onClick={this.props.handleLogout}>Log out</button>
+                <NavLink to="/login"><button type="button" onClick={this.props.handleLogout}>Log out</button></NavLink>
+            </HashRouter>
             </div>
         );
     }

f0b635e5f41c706e45570cb9c0dea7c12690ffc8
Roch <roch.sherriff@academytrainee.com>
2019-03-28 11:43:02 +0000

    experiment to interact with calendar import

--- src/TrainerCalendar.js
+++ src/TrainerCalendar.js
@@ -3,10 +3,18 @@ import './App.css';
 import Calendar from 'react-calendar';
 
 class TrainerCalendar extends Component{
+    constructor(props){
+        super(props);
+        this.state = {};
+    }
+    handleChange = (date) =>{
+        this.setState({date: date.toString()});
+    }
     render(){
         return(
             <div className="calendarPage">
-                <Calendar/>
+                <Calendar onChange={this.handleChange}/>
+                <p>{this.state.date}</p>
             </div>
         );
     }

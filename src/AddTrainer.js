7869c6ea51d0d73af8fb63d9ebfeac1373693f36
Roch <roch.sherriff@academytrainee.com>
2019-03-28 10:55:00 +0000

    Login functionality working with backend

--- src/AddTrainer.js
+++ src/AddTrainer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Axios from 'axios';
 
 class AddTrainer extends Component{
     constructor(props){
@@ -8,8 +9,16 @@ class AddTrainer extends Component{
     }
     handleSubmit =(e)=>{
         e.preventDefault();
-        let oldEmail = this.state.email;
-        this.setState({message: `New user created, email sent to ${oldEmail}`, formDisplay:false});
+        let body = { email:this.state.email, password:this.state.password, confirmPassword: this.state.password2, trainerName: this.state.name };
+        let url = "http://localhost:8080/createAccount";
+            var self = this;
+            Axios.post(url, body).then(function(response){
+                console.log(response);
+                self.setState({message: response.data, formDisplay:false});
+            }).catch(function(error){
+                console.log(error);
+                self.setState({message: "temp3", formDisplay:false});
+            });
     }
     updateState =(e)=>{
         const value = e.target.value;
@@ -30,6 +39,8 @@ class AddTrainer extends Component{
                 <input type="email" name="email" placeholder="bob@gmail.com" id="emailbox-addtrainer" onChange={this.updateState}/><br/>
                 Password:
                 <input type="password" name="password" placeholder="password1" id="passwordbox-addtrainer" onChange={this.updateState}/><br/>
+                Re-type Password:
+                <input type="password" name="password2" placeholder="password2" id="passwordbox2-addtrainer" onChange={this.updateState}/><br/>
                 <input type="submit" id="submit-button-addtrainer" value="Add this trainer"/>
             </form>
             <p>{this.state.message}</p>
